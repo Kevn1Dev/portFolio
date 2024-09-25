5e68e503e5fd1a03208c0fdbc85b1be6a55e1783
Kevn1Dev <kevinnicolas302@gmail.com>
2024-09-25 12:05:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// Obtener elementos del DOM
+const spanishOption = document.getElementById('spanish-option');
+const englishOption = document.getElementById('english-option');
+const greeting = document.getElementById('greeting');
+const description = document.getElementById('description');
+
+// Función para cambiar al idioma español
+spanishOption.addEventListener('click', function() {
+    greeting.textContent = 'Hola, bienvenido a mi portafolio';
+    description.textContent = 'Soy Diograzia Kevin Nicolás, un desarrollador Back-End. ¡Explora mis proyectos!';
+    toggleActiveLanguage(spanishOption, englishOption);
+});
+
+// Función para cambiar al idioma inglés
+englishOption.addEventListener('click', function() {
+    greeting.textContent = 'Hello, welcome to my portfolio';
+    description.textContent = 'I am Diograzia Kevin Nicolás, a Back-End developer. Explore my projects!';
+    toggleActiveLanguage(englishOption, spanishOption);
+});
+
+// Función para alternar la clase activa en los botones de idioma
+function toggleActiveLanguage(activeOption, inactiveOption) {
+    activeOption.classList.add('active');
+    inactiveOption.classList.remove('active');
+}
+
 // Si no esta completo el gmail y el nombre aparece un msj para rellenar
 const emailInput = document.getElementById('email');
 emailInput.addEventListener('input', function() {
