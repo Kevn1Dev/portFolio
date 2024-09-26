8acc4cec8694cd47568fa03e12e382b810ac4ca5
Kevn1Dev <kevinnicolas302@gmail.com>
2024-09-26 14:10:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// Obtener elementos del DOM
-const dropdownButton = document.getElementById('dropdown-button');
-const dropdownContent = document.getElementById('dropdown-content');
-
-// Función para mostrar/ocultar el menú desplegable
-dropdownButton.addEventListener('click', function() {
-  dropdownContent.classList.toggle('show');
-});
-
-// Cerrar el menú si se hace clic fuera de él
-window.addEventListener('click', function(e) {
-  if (!e.target.matches('#dropdown-button')) {
-      if (dropdownContent.classList.contains('show')) {
-          dropdownContent.classList.remove('show');
-      }
-  }
-});
 
 // Si no esta completo el gmail y el nombre aparece un msj para rellenar
 const emailInput = document.getElementById('email');
