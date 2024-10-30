cf78c032e00f1f855fc0e116a53016b6fc3bb948
Kevn1Dev <kevinnicolas302@gmail.com>
2024-10-30 14:53:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarLinks = document.querySelectorAll("header nav a");
+
+  navbarLinks.forEach(link => {
+    link.addEventListener("click", function (event) {
+      // Obtiene el id de destino desde el atributo href (eliminando el #)
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      // Si la sección objetivo existe, realiza el desplazamiento suave
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+        event.preventDefault(); // Previene el comportamiento predeterminado del enlace
+      }
+    });
+  });
+});
+
+
+
+
 // Si no esta completo el gmail y el nombre aparece un msj para rellenar
 const emailInput = document.getElementById('email');
 emailInput.addEventListener('input', function() {
